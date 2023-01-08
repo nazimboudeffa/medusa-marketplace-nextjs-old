ff183530a3d4c9d3d419c592831e80509d41037f
nazimboudeffa <boudeffa@gmail.com>
2023-01-08 16:59:48 +0100

    Changed content title

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
         <meta
           name="description"
-          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
+          content="Medusa Multi-Vendor"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
