3a4ad59948c06bf18b615772f9869ad9ef719ca8
nazimboudeffa <boudeffa@gmail.com>
2023-01-09 16:18:10 +0100

    Fixing axios get to products endpoint

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,20 +1,29 @@
-import axios from "axios";
-
-const getProducts = async () => {
-  try {
-    const res = await axios.get("http://192.168.0.45:9000/store/products");
-    const products = res.data.products;
-    console.log(products);
-    return products;
-  } catch (error) {
-    return error;
-  }
-};
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Products() {
+
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const response = await axios.get("http://192.168.0.45:9000/store/products");
+        const products = response.data.products;
+        console.log(products);
+        setProducts(products);
+    };
+
     return (
         <>
-            {getProducts()}
+        <button onClick={fetchProducts}>Fetch Products</button>
+            {
+            products.map((product: any) => {
+                return (
+                    <div key={product.id}>
+                        product.title
+                    </div>
+                )    
+            })
+            }
         </>
     )
 }
