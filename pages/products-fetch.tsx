19411164a082df85cbc281d7225423f831823177
nazimboudeffa <boudeffa@gmail.com>
2023-01-11 18:42:27 +0100

    Fixing error

--- pages/products-fetch.tsx
+++ pages/products-fetch.tsx
@@ -1,39 +1,41 @@
-// import React, { useState, useEffect } from 'react';
+{/*
+import React, { useState, useEffect } from 'react';
 
-// export default function Products() {
-//   const [data, setData] = useState(null);
-//   const [error, setError] = useState(null);
-//   const [loading, setLoading] = useState(true);
+export default function Products() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const response = await fetch('http://192.168.0.45:9000/store/products');
-//         const data = await response.json();
-//         setData(data);
-//       } catch (error) {
-//         setError(error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://192.168.0.45:9000/store/products');
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
 
-//     fetchData();
-//   }, []);
+    fetchData();
+  }, []);
 
-//   if (loading) {
-//     return <p>Loading...</p>;
-//   }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-//   if (error) {
-//     return <p>Error: {error.message}</p>;
-//   }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
-//   return (
-//     <ul>
-//       {data.products.map((product) => (
-//         <li key={product.id}>{product.title}</li>
-//       ))}
-//     </ul>
-//   );
-// }
+  return (
+    <ul>
+      {data.products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+*/}
